41c79b48a279e056730ce92f264908304b306633
TiagoMartins <TiagoMartins@192.168.207.121>
2017-04-11 00:52:05 +0100

    1.1

--- src/projeto/Topico.java
+++ src/projeto/Topico.java
@@ -7,12 +7,14 @@ package projeto;
 
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.Objects;
 
 /**
  *
  * @author Fábio
  */
 public class Topico{
+    
     private String nometopico;
     private String conteudo;
     private Calendar date;
@@ -26,13 +28,73 @@ public class Topico{
        
     }
     
-    public Topico(String top, String cont, Calendar dat)
-    {
+    public Topico(String top, String cont, Calendar dat){
         this.nometopico=top;
         this.conteudo=cont;
         this.date = dat;
         
     }
+
+    public String getNometopico() {
+        return nometopico;
+    }
+
+    public String getConteudo() {
+        return conteudo;
+    }
+
+    public Calendar getDate() {
+        return date;
+    }
+
+    public void setNometopico(String nometopico) {
+        this.nometopico = nometopico;
+    }
+
+    public void setConteudo(String conteudo) {
+        this.conteudo = conteudo;
+    }
+
+    public void setDate(Calendar date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "Topico{" + "nometopico=" + nometopico + ", conteudo=" + conteudo + ", date=" + date + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Topico other = (Topico) obj;
+        if (!Objects.equals(this.nometopico, other.nometopico)) {
+            return false;
+        }
+        if (!Objects.equals(this.conteudo, other.conteudo)) {
+            return false;
+        }
+        if (!Objects.equals(this.date, other.date)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
     
     
     
