64274a964e95b4e7fbc5c55658138714e6426e91
Fábio <Fbio@192.168.206.68>
2017-04-11 01:18:25 +0100

    1.0	versão

---
+++ src/projeto/ServerRMIIMP.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projeto;
+
+import java.rmi.server.UnicastRemoteObject;
+
+/**
+ *
+ * @author Fábio
+ */
+public class ServerRMIIMP extends UnicastRemoteObject implements ServerRMIInterface{
+    
+    
+}
